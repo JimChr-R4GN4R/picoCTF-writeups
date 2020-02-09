0942aa8b6343fba825e1f6648b579386ad0aadc7
R4GN4R <jimchr2001@gmail.com>
2020-02-09 10:13:57 -0500

    Update script.py

--- 2019/Cryptography/Mr-Worldwide (200 points)/script.py
+++ 2019/Cryptography/Mr-Worldwide (200 points)/script.py
@@ -4,7 +4,6 @@
 import json
 from subprocess import call
 
-
 try:
     import reverse_geocoder as rg 
 except:
@@ -12,6 +11,11 @@ except:
     print("################################# INSTALLING #################################")
     install_reverse_geocoder = call("pip3 install reverse_geocoder", shell=True)
     print("################################# INSTALLED #################################")
+
+
+
+
+
 
 # Put here your cords.
 cords = '(35.028309, 135.753082)(46.469391, 30.740883)(39.758949, -84.191605)(41.015137, 28.979530)(24.466667, 54.366669)(3.140853, 101.693207)_(9.005401, 38.763611)(-3.989038, -79.203560)(52.377956, 4.897070)(41.085651, -73.858467)(57.790001, -152.407227)(31.205753, 29.924526)'
@@ -29,6 +33,9 @@ cords= cords.split(' ')                                                         
 
 num_of_cords = len(cords) # 
 
+
+
+
 for i in range(0, num_of_cords): # repeat until all coordinates are covered.
 
     print('====================================================')
@@ -37,7 +44,8 @@ for i in range(0, num_of_cords): # repeat until all coordinates are covered.
     cord1 = float(now_pos[0])
     cord2 = float(now_pos[1])
 
-    def reverseGeocode(coordinates): 
+    def reverseGeocode(coordinates):
+
         result = rg.search(coordinates) 
         
         #########################################
@@ -85,7 +93,7 @@ for i in range(0, num_of_cords): # repeat until all coordinates are covered.
                                                                                  ###
         ############################################################################
 
-
+        print (Final_text)
     # Driver function 
     if __name__=="__main__": 
         
