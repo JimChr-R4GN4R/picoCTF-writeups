bf0ab26699395b37e208806b74e219176def9a76
R4GN4R <jimchr2001@gmail.com>
2020-03-07 16:01:36 -0500

    Update multi_tar_extractor.py

--- 2019/Forensics/like1000 (250 points)/multi_tar_extractor.py
+++ 2019/Forensics/like1000 (250 points)/multi_tar_extractor.py
@@ -3,6 +3,6 @@ from subprocess import call
 
 for i in range(1000,1,-1): # i = 1000, then i = 999 ....
 
-	shell = call("tar -xvf "+str(i)+".tar", shell=True)f # extract tar file by calling this bash command
+	shell = call("tar -xvf "+str(i)+".tar", shell=True)f # extract tar file by calling this bash command (tar -xvf 1000.tar)
   
 print("Finished")
